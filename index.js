62e7be01ad668c8e4f52c134fae9e3d8152ef8f0
Belinda <b.bowlyjung@gmail.com>
2016-12-03 15:03:21 +0700

    import bodyParser for use request body

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 "use strict";
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 9090;
+app.use(bodyParser.json());
+const todoItems = [
+    { title: 'Learn NodeJS', complete: false },
+    { title: 'Develop express app', complete: false }
+];
 app.get('/todo/list', (req, res) => {
-    const todoItems = [
-        { title: 'Learn NodeJS', complete: false },
-        { title: 'Develop express app', complete: false }
-    ];
     res.json(todoItems);
+});
+app.post('/todo/create', (req, res) => {
+    let todoItem = req.body;
+    todoItems.push(todoItem);
+    res.sendStatus(201);
 });
 app.listen(port, () => console.log(`Server listen on ${port}`));
--- index.ts
+++ index.ts
@@ -1,15 +1,24 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 9090;
+app.use(bodyParser.json());
+const todoItems = [
+    { title: 'Learn NodeJS', complete: false },
+    { title: 'Develop express app', complete: false }
+]
+
 
 app.get('/todo/list', (req, res) => {
-    const todoItems = [
-        { title: 'Learn NodeJS', complete: false },
-        { title: 'Develop express app', complete: false }
-    ]
-
     res.json(todoItems);
+});
+
+app.post('/todo/create', (req, res) => {
+    let todoItem = req.body;
+    todoItems.push(todoItem);
+
+    res.sendStatus(201);
 });
 
 app.listen(port, () => console.log(`Server listen on ${port}`));
